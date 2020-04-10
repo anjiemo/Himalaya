49be4a83b220f2fb6ebec90f7765d089346a6797
anjiemo <26957348416@qq.com>
2020-04-10 14:58:41 +0800

    处理播放器节目列表的关闭（第57集）。

--- app/src/main/java/com/smart/himalaya/views/MyPopWindow.java
+++ app/src/main/java/com/smart/himalaya/views/MyPopWindow.java
@@ -6,11 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.smart.himalaya.R;
 import com.smart.himalaya.base.BaseApplication;
 
 public class MyPopWindow extends PopupWindow {
+
+    private final View mPopView;
+    private TextView mCloseBtn;
+
     public MyPopWindow() {
         //设置它的宽高
         super(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -19,10 +24,21 @@ public class MyPopWindow extends PopupWindow {
         setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setOutsideTouchable(true);
         //载进来View
-        View popView = LayoutInflater.from(BaseApplication.getAppContext()).inflate(R.layout.pop_play_list, null);
+        mPopView = LayoutInflater.from(BaseApplication.getAppContext()).inflate(R.layout.pop_play_list, null);
         //设置内容
-        setContentView(popView);
+        setContentView(mPopView);
         //设置窗口进入和退出的动画
         setAnimationStyle(R.style.pop_animation);
+        initView();
+        initEvent();
+    }
+
+    private void initView() {
+        mCloseBtn = mPopView.findViewById(R.id.play_list_close_btn);
+    }
+
+    private void initEvent() {
+        //点击关闭以后，窗口消失
+        mCloseBtn.setOnClickListener(v -> dismiss());
     }
 }
