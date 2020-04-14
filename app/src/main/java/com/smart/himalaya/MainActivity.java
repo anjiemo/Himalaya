60cc32a8db319f2d1051265ac746fe9622ca8692
anjiemo <26957348416@qq.com>
2020-04-14 12:00:45 +0800

    主界面播放数据通知回来（第80集）。

--- app/src/main/java/com/smart/himalaya/MainActivity.java
+++ app/src/main/java/com/smart/himalaya/MainActivity.java
@@ -2,6 +2,9 @@ package com.smart.himalaya;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -9,17 +12,30 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.smart.himalaya.adapters.IndicatorAdapter;
 import com.smart.himalaya.adapters.MainContentAdapter;
+import com.smart.himalaya.interfaces.IPlayerCallback;
+import com.smart.himalaya.presenters.PlayerPresenter;
+import com.smart.himalaya.utils.LogUtil;
+import com.smart.himalaya.views.RoundRectImageView;
+import com.ximalaya.ting.android.opensdk.model.track.Track;
+import com.ximalaya.ting.android.opensdk.player.service.XmPlayListControl;
 
 import net.lucode.hackware.magicindicator.MagicIndicator;
 import net.lucode.hackware.magicindicator.ViewPagerHelper;
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
 
-public class MainActivity extends FragmentActivity {
+import java.util.List;
+
+public class MainActivity extends FragmentActivity implements IPlayerCallback {
 
     private static final String TAG = "MainActivity";
     private MagicIndicator mMagicIndicator;
     private ViewPager mContentPager;
     private IndicatorAdapter mIndicatorAdapter;
+    private RoundRectImageView mRoundRectImageView;
+    private TextView mHeaderView;
+    private TextView mSubTitle;
+    private ImageView mPlayControl;
+    private PlayerPresenter mPlayerPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +43,25 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
         initView();
         initEvent();
+        initPresenter();
+    }
 
+    private void initPresenter() {
+        mPlayerPresenter = PlayerPresenter.getPlayerPresenter();
+        mPlayerPresenter.registerViewCallback(this);
     }
 
     private void initEvent() {
-        mIndicatorAdapter.setOnIndicatorTapClickListener(new IndicatorAdapter.OnIndicatorTabClickListener() {
-            @Override
-            public void onTabClick(int index) {
-                Log.d(TAG, "onTabClick: ==========" + "click index is ====>" + index);
-                if (mContentPager != null) {
-                    mContentPager.setCurrentItem(index);
-                }
+        mIndicatorAdapter.setOnIndicatorTapClickListener(index -> {
+            Log.d(TAG, "onTabClick: ==========" + "click index is ====>" + index);
+            if (mContentPager != null) {
+                mContentPager.setCurrentItem(index);
             }
         });
     }
 
     private void initView() {
-        mMagicIndicator = (MagicIndicator) findViewById(R.id.main_indicator);
+        mMagicIndicator = findViewById(R.id.main_indicator);
         mMagicIndicator.setBackgroundColor(getResources().getColor(R.color.main_color));
         //创建indicator的适配器
         mIndicatorAdapter = new IndicatorAdapter(this);
@@ -51,9 +69,8 @@ public class MainActivity extends FragmentActivity {
         commonNavigator.setAdjustMode(true);//设置调整模式(自我调节，平分位置)
         commonNavigator.setAdapter(mIndicatorAdapter);
 
-
         //ViewPager
-        mContentPager = (ViewPager) findViewById(R.id.content_pager);
+        mContentPager = findViewById(R.id.content_pager);
 
         //创建内容适配器
         FragmentManager supportFragmentManager = getSupportFragmentManager();
@@ -62,7 +79,87 @@ public class MainActivity extends FragmentActivity {
         mContentPager.setAdapter(mainContentAdapter);
         //把ViewPager和indicator绑定到一起
         mMagicIndicator.setNavigator(commonNavigator);
-        ViewPagerHelper.bind(mMagicIndicator,mContentPager);
+        ViewPagerHelper.bind(mMagicIndicator, mContentPager);
+
+        //播放控制相关的
+        mRoundRectImageView = findViewById(R.id.main_track_cover);
+        mHeaderView = findViewById(R.id.main_head_title);
+        mSubTitle = findViewById(R.id.main_sub_title);
+        mPlayControl = findViewById(R.id.main_play_control);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPlayerPresenter != null) {
+            mPlayerPresenter.unRegisterViewCallback(this);
+        }
+    }
+
+    @Override
+    public void onPlayStart() {
+
+    }
+
+    @Override
+    public void onPlayPause() {
+
+    }
+
+    @Override
+    public void onPlayStop() {
+
+    }
+
+    @Override
+    public void onPlayError(Track track) {
+
+    }
+
+    @Override
+    public void onPrePlay(Track track) {
+
+    }
+
+    @Override
+    public void onListLoad(List<Track> list) {
+
+    }
+
+    @Override
+    public void onPlayModeChange(XmPlayListControl.PlayMode playMode) {
+
+    }
+
+    @Override
+    public void onProgressChange(int currentProgress, int total) {
+
+    }
+
+    @Override
+    public void onAdLoading() {
+
+    }
+
+    @Override
+    public void onAdFinished() {
+
+    }
+
+    @Override
+    public void onTrackUpdate(Track track, int playIndex) {
+        if (track != null) {
+            String trackTitle = track.getTrackTitle();
+            String nickname = track.getAnnouncer().getNickname();
+            String coverUrlMiddle = track.getCoverUrlMiddle();
+            LogUtil.d(TAG, "trackTitle --- > " + trackTitle);
+            LogUtil.d(TAG, "nickname --- > " + nickname);
+            LogUtil.d(TAG, "coverUrlMiddle --- > " + coverUrlMiddle);
+        }
+    }
+
+    @Override
+    public void updateListOrder(boolean isReverse) {
 
     }
 }
