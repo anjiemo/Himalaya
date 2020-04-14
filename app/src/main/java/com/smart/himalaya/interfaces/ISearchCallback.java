ab30258d76ddc5cfe9498bfd31b14e990f31216a
anjiemo <26957348416@qq.com>
2020-04-14 14:55:45 +0800

    定义搜索的接口（第88集）。

---
+++ app/src/main/java/com/smart/himalaya/interfaces/ISearchCallback.java
@@ -0,0 +1,39 @@
+package com.smart.himalaya.interfaces;
+
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.word.HotWord;
+import com.ximalaya.ting.android.opensdk.model.word.QueryResult;
+
+import java.util.List;
+
+public interface ISearchCallback {
+
+    /**
+     * 搜索结果的回调方法
+     *
+     * @param result
+     */
+    void onSearchResultLoaded(List<Album> result);
+
+    /**
+     * 获取推荐的热词的结果回调方法
+     *
+     * @param hotWordList
+     */
+    void onHotWordLoaded(List<HotWord> hotWordList);
+
+    /**
+     * 加载更多的结果返回
+     *
+     * @param result 结果
+     * @param isOkay true表示加载更多成功，false表示没有更多
+     */
+    void onLoadMoreResult(List<Album> result, boolean isOkay);
+
+    /**
+     * 联想关键字的结果回调方法
+     *
+     * @param keyWordList
+     */
+    void onRecommendWordLoaded(List<QueryResult> keyWordList);
+}
