aa6158a1ab6b85bbc13187ad5b0a711c73edc914
anjiemo <26957348416@qq.com>
2020-04-15 12:46:04 +0800

    定义订阅的接口（第107集）。

---
+++ app/src/main/java/com/smart/himalaya/interfaces/ISubscriptionCallback.java
@@ -0,0 +1,29 @@
+package com.smart.himalaya.interfaces;
+
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+
+import java.util.List;
+
+public interface ISubscriptionCallback {
+
+    /**
+     * 调用添加的时候，去通知UI结果
+     *
+     * @param isSuccess
+     */
+    void onAddResult(boolean isSuccess);
+
+    /**
+     * 删除订阅的回调方法
+     *
+     * @param isSuccess
+     */
+    void onDeleteResult(boolean isSuccess);
+
+    /**
+     * 订阅专辑加载的结果回调方法
+     *
+     * @param albums
+     */
+    void onSubscriptionsLoaded(List<Album> albums);
+}
---
+++ app/src/main/java/com/smart/himalaya/interfaces/ISubscriptionPresenter.java
@@ -0,0 +1,29 @@
+package com.smart.himalaya.interfaces;
+
+import com.smart.himalaya.base.IBasePresenter;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+
+/**
+ * 订阅我们一般有上限，比如说不能超过100个
+ */
+public interface ISubscriptionPresenter extends IBasePresenter<ISubscriptionCallback> {
+
+    /**
+     * 添加订阅
+     *
+     * @param album
+     */
+    void addSubscription(Album album);
+
+    /**
+     * 删除订阅
+     *
+     * @param album
+     */
+    void deleteSubscription(Album album);
+
+    /**
+     * 获取订阅列表
+     */
+    void getSubscriptionList();
+}
